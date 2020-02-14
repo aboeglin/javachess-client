003806f2f36837dd36954eee917108274994a919
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2020-02-14 18:06:27 +0100

    Add some more tests to SquareContainer

--- src/components/ChessGame/SquareContainer.spec.js
+++ src/components/ChessGame/SquareContainer.spec.js
@@ -5,3 +5,9 @@ standardComponentTest(SquareContainer, {
   pieces: [],
   square: { color: "WHITE", x: "b", y: "3", highlighted: false }
 });
+
+standardComponentTest(SquareContainer, {
+  selection: { x: "a", y: "2", color: "WHITE" },
+  pieces: [{ x: "a", y: "3", type: "PAWN", color: "WHITE" }],
+  square: { color: "WHITE", x: "a", y: "3", highlighted: false }
+});
